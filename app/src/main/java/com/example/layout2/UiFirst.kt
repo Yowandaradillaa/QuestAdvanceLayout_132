30cd5f13ff9f3950220b02221b27862307b9d092
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-24 01:25:17 +0700

    Menambahkan kolom teks nama di card kedua

--- app/src/main/java/com/example/layout2/UiFirst.kt
+++ app/src/main/java/com/example/layout2/UiFirst.kt
@@ -90,6 +90,16 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                         .size(100.dp)
                         .padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.nama2),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
             }
         }
 
