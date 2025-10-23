62e03ac8e45d45b279a1024f4f9764d5c18670aa
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-23 15:53:07 +0700

    Menambahkan jarak antar elemen header dan kartu

--- app/src/main/java/com/example/layout2/UiFirst.kt
+++ app/src/main/java/com/example/layout2/UiFirst.kt
@@ -32,5 +32,6 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(25.dp))
 
     }
