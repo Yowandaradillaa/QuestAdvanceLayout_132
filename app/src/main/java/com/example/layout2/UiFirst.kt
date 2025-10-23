96ebf381202ecb03a17c454275aed0400015be61
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-24 01:36:21 +0700

    Menambahkan kolom teks nama di card ketiga

--- app/src/main/java/com/example/layout2/UiFirst.kt
+++ app/src/main/java/com/example/layout2/UiFirst.kt
@@ -135,6 +135,16 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                         .size(100.dp)
                         .padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
             }
         }
 
