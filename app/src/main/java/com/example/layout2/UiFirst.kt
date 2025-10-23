8de78b6e74ae406aedf0a4bd7ab84cd7e2191844
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-23 15:52:16 +0700

    Menambahkan teks untuk prodi dan universitas

--- app/src/main/java/com/example/layout2/UiFirst.kt
+++ app/src/main/java/com/example/layout2/UiFirst.kt
@@ -23,3 +23,14 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
+
+    }
